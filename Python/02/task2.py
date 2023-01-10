12c680b9e0236af1430ffb952fbb8990cfd247d6
Andrew Wasef <86547011+AndrewWasef@users.noreply.github.com>
2023-01-10 18:15:30 +0200

    Update task2.py

--- Python/02/task2.py
+++ Python/02/task2.py
@@ -5,7 +5,7 @@ def leap_year(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
     else:
-        print ( year + "  is not a leap year")
+        return False
  
 year = int(input("PLease Enter The Year you want to check if it's a leap : \n"))
 
